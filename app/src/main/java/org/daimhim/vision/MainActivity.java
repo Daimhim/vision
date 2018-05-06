f2feae680a01db60a602d69fc95c6534f9d0246d
Daimhim <daimhim@163.com>
2018-05-06 22:54:46 +0800

    init

--- app/src/main/java/org/daimhim/vision/MainActivity.java
+++ app/src/main/java/org/daimhim/vision/MainActivity.java
@@ -3,11 +3,14 @@ package org.daimhim.vision;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.daimhim.weakhandler.WeakHandler;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new WeakHandler();
     }
 }
