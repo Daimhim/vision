ddfe0a4127bbe8ea62fbc7a7dcefcc0c5f9d44d1
Daimhim <daimhim@163.com>
2018-05-30 15:56:13 +0800

    One Key Payment

---
+++ onekeypayment/src/main/java/org/daimhim/onekeypayment/ToPay.java
@@ -0,0 +1,15 @@
+package org.daimhim.onekeypayment;
+
+/**
+ * 项目名称：org.daimhim.onekeypayment
+ * 项目版本：vision
+ * 创建时间：2018.05.30 15:41
+ * 修改人：Daimhim
+ * 修改时间：2018.05.30 15:41
+ * 类描述：
+ * 修改备注：
+ *
+ * @author：Daimhim
+ */
+public class ToPay {
+}
---
+++ onekeypayment/src/main/java/org/daimhim/onekeypayment/model/PaymentRequest.java
@@ -0,0 +1,15 @@
+package org.daimhim.onekeypayment.model;
+
+/**
+ * 项目名称：org.daimhim.onekeypayment.model
+ * 项目版本：vision
+ * 创建时间：2018.05.30 15:50
+ * 修改人：Daimhim
+ * 修改时间：2018.05.30 15:50
+ * 类描述：
+ * 修改备注：
+ *
+ * @author：Daimhim
+ */
+public class PaymentRequest {
+}
